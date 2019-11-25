840758ba9394ae5d182935489c8163e35889c39e
郑尚姿 <33643927+zhengshangzi@users.noreply.github.com>
2019-11-25 17:24:59 +0800

    Add files via upload

--- houduan/src/container/Score/List.js
+++ houduan/src/container/Score/List.js
@@ -1,9 +1,82 @@
 import React, { Component } from 'react'
+import { Table } from 'antd';
+const data = [
+    {
+      key: '1',
+      userName: '郑尚姿',
+      scoreRank:'1',
+      sum:100,
+      updateTime:'1999-02-05'
+    },
+    {
+        key: '1',
+        userName: '郑尚姿',
+        scoreRank:'1',
+        sum:100,
+        updateTime:'1999-02-05'
+      },
+      {
+        key: '1',
+        userName: '郑尚姿',
+        scoreRank:'1',
+        sum:100,
+        updateTime:'1999-02-05'
+      },
+      {
+        key: '1',
+        userName: '郑尚姿',
+        scoreRank:'1',
+        sum:100,
+        updateTime:'1999-02-05'
+      },
+      {
+        key: '1',
+        userName: '郑尚姿',
+        scoreRank:'1',
+        sum:100,
+        updateTime:'1999-02-05'
+      },
+     
+  
+  ];
+  const columns = [
+    {
+      title: '用户名',
+      dataIndex: 'userName',
+      key: 'userName',
+    },
+    {
+      title: '更新时间',
+      dataIndex: 'updateTime',
+      key: 'updateTime',
+    },
+    {
+      title: '总分',
+      dataIndex: 'sum',
+      key: 'sum',
+    },
+    {
+        title: '排名',
+        dataIndex: 'scoreRank',
+        key: 'scoreRank',
+      },
+      {
+        title: '个人积分',
+        key: 'action',
+        render: (text, record) => (
+          <span>
+            <a>个人积分明细</a>
+          </span>
+        ),
+      },
+  ];
 
 export default class List extends Component {
     render() {
         return (
-            <div>2
+            <div>
+                 <Table columns={columns} dataSource={data} style={{marginLeft:'5%',marginRight:'5%',marginTop:"4%"}} pagination={{defaultPageSize:4}}/>
+
                 
             </div>
         )
--- houduan/src/container/Score/Manage.js
+++ houduan/src/container/Score/Manage.js
@@ -1,9 +1,91 @@
 import React, { Component } from 'react'
-
+import { Table } from 'antd';
+const data = [
+    {
+      key: '1',
+      Uimage:'1.png',
+      Uname: '郑尚姿',
+      Uphone:'15031530670',
+      Usex:'女',
+      Ubirthday:'1999-02-05'
+    },
+    {
+        key: '2',
+        Uimage:'1.png',
+        Uname: '郑尚姿',
+        Uphone:'15031530670',
+        Usex:'女',
+        Ubirthday:'1999-02-05'
+      },
+      {
+        key: '3',
+        Uimage:'1.png',
+        Uname: '郑尚姿',
+        Uphone:'15031530670',
+        Usex:'女',
+        Ubirthday:'1999-02-05'
+      },
+      {
+        key: '4',
+        Uimage:'1.png',
+        Uname: '郑尚姿',
+        Uphone:'15031530670',
+        Usex:'女',
+        Ubirthday:'1999-02-05'
+      },
+      {
+        key: '5',
+        Uimage:'1.png',
+        Uname: '郑尚姿',
+        Uphone:'15031530670',
+        Usex:'女',
+        Ubirthday:'1999-02-05'
+      },
+     
+  
+  ];
+  const columns = [
+    {
+        title: '头像',
+        dataIndex: 'Uimage',
+        key: 'Uimage',
+    },
+    {
+      title: '用户名',
+      dataIndex: 'Uname',
+      key: 'Uname',
+    },
+    {
+      title: '手机号',
+      dataIndex: 'Uphone',
+      key: 'Uphone',
+    },
+    {
+      title: '性别',
+      dataIndex: 'Usex',
+      key: 'Usex',
+    },
+    {
+        title: '生日',
+        dataIndex: 'Ubirthday',
+        key: 'Ubirthday',
+      },
+      {
+        title: '操作',
+        key: 'action',
+        render: (text, record) => (
+          <span>
+            <a><button style={{backgroundColor:'white',border:'1px solid grey',padding:'5px 10px'}}>移除</button></a>
+          </span>
+        ),
+      },
+  ];
 export default class Manage extends Component {
     render() {
         return (
-            <div>1
+            <div>
+                <Table columns={columns} dataSource={data} style={{marginLeft:'5%',marginRight:'5%',marginTop:"4%"}} pagination={{defaultPageSize:4}}/>
+
                 
             </div>
         )
