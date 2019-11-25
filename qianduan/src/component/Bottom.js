22eff32a4dbe8b022917353d92d33d95238639ed
wangyn <928069834@qq,com>
2019-11-25 15:11:22 +0800

    wyn

--- qianduan/src/component/Bottom.js
+++ qianduan/src/component/Bottom.js
@@ -1,85 +1,45 @@
-import React, { Component } from 'react'
-import { TabBar } from 'antd-mobile';
+import React, { Component } from 'react';
+import {BrowserRouter as Router,Route,Link} from 'react-router-dom';
 import Home from '../containers/Home/Home';
 import Block from '../containers/Block/Block';
 import Message from '../containers/Message/Message';
 import My from '../containers/My/My';
 
-
 export default class Bottom extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            selectedTab: 'home',
-        };
-    }
-    render() {
-        return (
-        <div style={{ position: 'fixed', height: '100%', width: '100%', top: 0 }}>
-            
-        <TabBar
-          unselectedTintColor="#949494"
-          tintColor="#33A3F4"
-          barTintColor="white"
-        >
-            <TabBar.Item
-            title="首页"
-            key="Home"
-            icon={<i style={{'fontsize':'22px'}} className='iconfont icon-xuanzhongshangcheng'></i>}
-            selectedIcon={<i style={{'fontsize':'22px'}} className='iconfont icon-xuanzhongshangcheng'></i>}
-            selected={this.state.selectedTab === 'home'}
-            onPress={() => {
-              this.setState({
-                selectedTab: 'home',
-              });
-            }}
-            >
-                <Home/>
-            </TabBar.Item>
-            <TabBar.Item
-            title="论坛"
-            key="Block"
-            icon={<i style={{'fontsize':'22px'}} className='iconfont icon-bbs'></i>}
-            selectedIcon={<i style={{'fontsize':'22px'}} className='iconfont icon-bbs'></i>}
-            selected={this.state.selectedTab === 'block'}
-            onPress={() => {
-              this.setState({
-                selectedTab: 'block',
-              });
-            }}
-            >
-                <Block/>
-            </TabBar.Item>
-            <TabBar.Item
-            title="消息"
-            key="Message"
-            icon={<i style={{'fontsize':'22px'}} className='iconfont icon-xiaoxi-control'></i>}
-            selectedIcon={<i style={{'fontsize':'22px'}} className='iconfont icon-xiaoxi-control'></i>}
-            selected={this.state.selectedTab === 'message'}
-            onPress={() => {
-              this.setState({
-                selectedTab: 'message',
-              });
-            }}
-            >
-                <Message/>
-            </TabBar.Item>
-            <TabBar.Item
-            title="我的"
-            key="My"
-            icon={<i style={{'fontsize':'22px'}} className='iconfont icon-my'></i>}
-            selectedIcon={<i style={{'fontsize':'22px'}} className='iconfont icon-my'></i>}
-            selected={this.state.selectedTab === 'my'}
-            onPress={() => {
-              this.setState({
-                selectedTab: 'my',
-              });
-            }}
-            >
-                <My/>
-            </TabBar.Item>
-        </TabBar>
-      </div>
-        )
-    }
+  render() {
+    return (
+      <Router>
+        <div style={{position:"absolute",bottom:"0",width:"100%",height:"70px",color:"#2278c9"}}>
+        <div className="bottom">
+            <Link to="/">
+              <div className="iconfont icon-xuanzhongshangcheng"></div>
+              <p>首页</p>
+            </Link>
+          </div>
+          <div className="bottom">
+            <Link to="/block">
+              <div className="iconfont icon-bbs"></div>
+              <p>论坛</p>
+            </Link>
+          </div>
+          <div className="bottom">
+            <Link to="/message">
+              <div className="iconfont icon-xiaoxi-control"></div>
+              <p>消息</p>
+            </Link>
+          </div>
+          <div className="bottom">
+            <Link to="/my">
+              <div className="iconfont icon-my"></div>
+              <p>我的</p>
+            </Link>
+          </div>
+        </div>
+        <Route exact path='/' component={Home}/>
+        <Route path='/block' component={Block}/>
+        <Route path='/message' component={Message}/>
+        <Route path='/my' component={My}/>
+      </Router>
+    )
+  }
 }
--- qianduan/src/containers/Home/Home.js
+++ qianduan/src/containers/Home/Home.js
@@ -5,6 +5,31 @@ export default class Home extends Component {
         return (
             <div>
                 Home
+                {/* <Carousel
+                autoplay={false}
+                infinite
+                beforeChange={(from, to) => console.log(`slide from ${from} to ${to}`)}
+                afterChange={index => console.log('slide to', index)}
+                >
+                {this.state.data.map(val => (
+                    <a
+                    key={val}
+                    href="http://www.alipay.com"
+                    style={{ display: 'inline-block', width: '100%', height: this.state.imgHeight }}
+                    >
+                    <img
+                        src={`https://zos.alipayobjects.com/rmsportal/${val}.png`}
+                        alt=""
+                        style={{ width: '100%', verticalAlign: 'top' }}
+                        onLoad={() => {
+                        // fire window resize event to change height
+                        window.dispatchEvent(new Event('resize'));
+                        this.setState({ imgHeight: 'auto' });
+                        }}
+                    />
+                    </a>
+                ))}
+                </Carousel> */}
             </div>
         )
     }
