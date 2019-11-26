c859ff0f2127971ebdeab386926a5d8a8dd3b071
YangDiaa <33643005+YangDiaa@users.noreply.github.com>
2019-11-26 17:41:21 +0800

    Add files via upload

--- houduan/src/container/Material/Method.js
+++ houduan/src/container/Material/Method.js
@@ -1,6 +1,8 @@
-import React, { Component } from 'react'
-import { Table } from 'antd'
+import React, { useState } from 'react';
+import { Table} from 'antd';
+import Model from './Model'
 import './Material.css'
+
 const data = [
     {
         key: '1',
@@ -28,10 +30,9 @@ const data = [
         Mcontent:'01.txt',
         Mimage:'01.png',
         Method:'01.doc'
-    },
-    
+    } 
 ];
-  const columns = [
+const columns = [
     {
         title: '编号',
         dataIndex: 'Mid',
@@ -62,7 +63,6 @@ const data = [
         dataIndex: 'Method',
         key: 'Method',
     },
-    
     {
         title: '操作',
         key: 'action',
@@ -72,26 +72,32 @@ const data = [
                 <a><button style={{backgroundColor:'white',border:'1px solid #dfdfdf',padding:'5px 10px'}}>移除</button></a>
             </span>
         ),
-    },
-];
-export default class Method extends Component {
-    render() {
-        return (
-            <div>
-                <div className="Materialbox1">
-                     <div className='Materialsearch'>
-                        <input  className='MaterialBar' placeholder="请输入要查找的关键字"/>
-                        <i className="iconfont icon-sousuo da"/>
-                     </div>
-                </div>
-                <div className="Materialbox2">
-                    <p style={{fontSize:'22px',fontWeight:'bold'}}>爱游不错游攻略管理
-                        <i style={{marginLeft:'30px'}}className="iconfont icon-wuuiconxiangjifangda"/>
-                        <span style={{fontWeight:'lighter',fontSize:'20px',marginLeft:'10px'}}>添加</span>                        
-                    </p>
-                </div>
-                <Table columns={columns} dataSource={data} style={{marginLeft:'5%',marginRight:'5%',marginTop:"1%"}} pagination={{defaultPageSize:4}}/>   
-            </div>
-        )
     }
+];
+
+export default function Method(){     
+    const [isShowPop,changeShowPop] = useState(false);
+    function handleInClick(){
+        changeShowPop(!isShowPop);
+    }
+    return (
+        <div style={{position:'relative'}}>
+            <div className="Materialbox1">
+                <div className='Materialsearch'>
+                    <input  className='MaterialBar' placeholder="请输入要查找的关键字"/>
+                    <i className="iconfont icon-sousuo da"/>
+                </div>
+            </div>
+            <div className="Materialbox2">
+                <p style={{fontSize:'22px',fontWeight:'bold'}}>爱游不错游攻略管理
+                    <i style={{marginLeft:'30px'}}className="iconfont icon-wuuiconxiangjifangda"/>
+                    {(isShowPop == true)?<Model isShow={isShowPop} closeModel={handleInClick}/>:null}
+                    <button style={{background:'white',border:'none',fontSize:'20px',marginLeft:'10px'}} onClick={handleInClick}>添加</button>
+                </p>
+            </div>
+            <Table columns={columns} dataSource={data} style={{marginLeft:'5%',marginRight:'5%',marginTop:"0.6%"}} pagination={{defaultPageSize:4}}/>
+            <div id={"model-root"} style={{position:"absolute",top:'10px',zIndex:'100',}}></div>
+            <div id="popLayer" style={{position: "absolute",display:"none",left:"0",top:"0",right:'0',zIndex:"0",background:"white",opacity:"0.8",filter: "alpha(opacity=80)",/* 只支持IE6、7、8、9 */}} ></div>
+        </div>
+    )
 }
