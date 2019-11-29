2710c6029d16cacdd1619a4498628fcfba79ae7a
杜瑜萌 <33643019+duyumengdym@users.noreply.github.com>
2019-11-29 10:43:03 +0800

    Update index.js

--- houduan/routes/index.js
+++ houduan/routes/index.js
@@ -14,9 +14,86 @@ router.get('/block', function(req, res, next) {
 router.get('/block/post', function(req, res, next) {
   res.render('Block/post', { title: 'Express' });
 });
+// 展示话题页
 router.get('/block/topic', function(req, res, next) {
-  res.render('Block/topic', { title: 'Express' });
+  var con=mysql.createConnection(dbconfig);
+  con.connect();
+  con.query("select * from topic",function (err,result) {
+    if(err){
+      console.log(err);
+    }else{
+      res.render("Block/topic",{topicList:result});
+    }
+  });
 });
+//删除话题
+router.get("/deltopic",function (req,res,next) {
+  var topicId=req.query.id;
+  var con=mysql.createConnection(dbconfig);
+  con.connect();
+  con.query("delete from topic where topicId=?",[topicId],function (err,result) {
+    if(err){
+      console.log(err);
+    }else{
+      res.render("Block/topicdel",{topicList:result});
+    }
+  });
+})
+//添加话题
+router.post('/addtopic',function (req,res,next) {
+    let now='';
+    function getDate(){
+        var myDate = new Date();
+        var year = myDate.getFullYear();
+        var month = myDate.getMonth() + 1;
+        var date = myDate.getDate();
+        now = year + '-' + conver(month) + "-" + conver(date);
+    }
+    function conver(s) {
+        return s < 10 ? '0' + s : s;
+    }
+    getDate();
+    var name=req.body.name;
+    var content=req.body.content;
+    var con=mysql.createConnection(dbconfig);
+    con.connect();
+    con.query("insert into topic(topicContent,adminUsername,topicTime) values(?,?,?)",[content,name,now],function (err,result) {
+    if(err){
+      console.log(err);
+    }else{
+      res.render("Block/topicadd",{topicList:result});
+    }
+  });
+});
+//找到id值
+let topicId=0;
+router.post("/topic",function (req,res,next) {
+  topicId=req.body.topic;
+  var con=mysql.createConnection(dbconfig);
+  con.connect();
+  console.log(topicId);
+  con.query("select * from topic",function (err,result) {
+    if(err){
+      console.log(err);
+    }else{
+      res.render("Block/bianji",{topicList:result});
+    }
+  });
+})
+// 编辑话题页面
+router.post("/update",function (req,res,next) {
+  var adminUsername=req.body.name1;
+  var topicContent=req.body.content1;
+  var con=mysql.createConnection(dbconfig);
+  con.connect(); 
+  con.query("update topic set topicContent=?,adminUsername=? where topicId=?",[topicContent,adminUsername,topicId],function (err,result) {
+    if(err){
+      console.log(err);
+    }else{
+      res.render("Block/topicnew",{topicList:result});
+    }
+  });
+})
 router.get('/score', function(req, res, next) {
   res.render('Score/score', { title: 'Express' });
 });
