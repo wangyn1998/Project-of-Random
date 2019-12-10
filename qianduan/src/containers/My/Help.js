37c0fd1f73fb25a454058f1bd5784d129cbac5fa
郑尚姿 <33643927+zhengshangzi@users.noreply.github.com>
2019-12-10 08:45:55 +0800

    Add files via upload

--- qianduan/src/containers/My/Help.js
+++ qianduan/src/containers/My/Help.js
@@ -17,8 +17,11 @@ export default class Help extends Component {
                     onLeftClick={()=>window.location.href='/my'}
                     >帮助反馈
                 </NavBar>
-                <div className='help' onClick={this.a.bind(this)}>
-                    当前版本暂不支持该功能，相信你自己是可以的
+                <div className='help'  onClick={this.a.bind(this)}>
+                    <div style={{color:'black',fontSize:'24px',paddingTop:'10%',margin:'0 auto',width:'60%',height:'70px',textAlign:'center'}}>当前版本暂不支持该功能。</div>
+                    <div style={{color:'black',fontSize:'24px',margin:'0 auto',width:'60%',height:'40px',textAlign:'center'}}>相信你自己是可以的</div>
+                    <div style={{color:'black',fontSize:'24px',margin:'0 auto',width:'60%',height:'100px',textAlign:'center'}}>加油！</div>
+
                 </div>
             </div>
         )
